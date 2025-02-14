4b888d75c91c78f63d18a5a68d0bb3679ae53a85
Lean Lin <xphoenixx32@gmail.com>
2025-02-14 11:56:40 +0800

    Update raffling_number.py

--- raffling_number.py
+++ raffling_number.py
@@ -36,7 +36,7 @@ st.set_page_config(page_title="Lottery Raffling Tool", layout="centered")
 
 # Title and description
 st.markdown("<h4 style='text-align: center; color: #333;'>🙌 Welcome, future billionaires!</h2>", unsafe_allow_html=True)
-st.markdown("###### 💰 *Lottery Tool*")
+st.markdown("#### 💰 *Lottery Tool*")
 st.markdown("##### Choose a Sampling Method")
 
 # Use a selection box instead of a radio button
